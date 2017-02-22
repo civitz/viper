7a5a85a808ff583b3463792eb62fb3402fca19e4
robypiwa <robypiwa@gmail.com>
2017-02-23 00:46:02 +0100

    Better README, more javadoc in examples #8 #9

--- README.md
+++ README.md
@@ -48,7 +48,71 @@ public MyApplicationLogic {
 	String firstProperty;
 	
 	void myMethod(){
-		// use the property
+		// use the property, Luke
 	}
 }
+```
+
+## A more complex example
+
+You can tune the generated bean with more annotations:
+
+```java
+package tests;
+import java.util.function.Predicate;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import civitz.viper.CdiConfiguration;
+
+/*
+ * You can specify one or more annotations for the Configuration Bean.
+ * In this case, Configuration Bean will have the ApplicationScoped qualifier
+ */
+@CdiConfiguration.PassAnnotations({ApplicationScoped.class})
+@CdiConfiguration(propertiesPath="/opt/generatortests/config.properties")
+public enum CompleteEnum {
+
+	FIRST_PROPERTY("my.particular.key", s -> Ints.tryParse(s) != null),
+	SECOND_PROPERTY("my.other.key", s -> s.length() >= 10),
+
+	/*
+	 * You can mark a specific enum constant to be the null value instead of the
+	 * first one
+	 */
+	@CdiConfiguration.KeyNullValue
+	PLEASE_IGNORE_ME("");
+
+	String key;
+	Predicate<String> validator;
+
+	CompleteEnum(String key) {
+		this(key, s -> true);
+	}
+
+	CompleteEnum(String key, Predicate<String> validator) {
+		this.key = key;
+		this.validator = validator;
+	}
+
+	/*
+	 * You can specify a method to get a validator for a property: if the
+	 * property is not valid, the Configuration Bean will throw an
+	 * IllegalArgumentException in the initialization method.
+	 */
+	@CdiConfiguration.ConfigValidator
+	public Predicate<String> getValidator() {
+		return validator;
+	}
+
+	/*
+	 * You can specify a method to get a particular string value as a property
+	 * key instead of the generic enum constant name in lowercase.
+	 */
+	@CdiConfiguration.KeyString
+	public String getKeyString() {
+		return key;
+	}
+}
+
 ```
--- generatortests/src/test/java/generatortests/CompleteEnum.java
+++ generatortests/src/test/java/generatortests/CompleteEnum.java
@@ -4,23 +4,55 @@ import java.util.function.Predicate;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import com.google.common.primitives.Ints;
+
 import civitz.viper.CdiConfiguration;
 
-@CdiConfiguration(propertiesPath="/opt/generatortests/config.properties")
+/*
+ * You can specify one or more annotations for the Configuration Bean.
+ * In this case, Configuration Bean will have the ApplicationScoped qualifier
+ */
 @CdiConfiguration.PassAnnotations(ApplicationScoped.class)
+@CdiConfiguration(propertiesPath="/opt/generatortests/config.properties")
 public enum CompleteEnum {
-	@CdiConfiguration.KeyNullValue
-	NULL,
-	FIRST_PROPERTY,
-	SECOND_PROPERTY;
+	FIRST_PROPERTY("my.particular.key", s -> Ints.tryParse(s) != null),
+	SECOND_PROPERTY("my.other.key", s -> s.length() >= 10),
 
-	@CdiConfiguration.ConfigValidator
-	public Predicate<String> getValidator(){
-		return s -> true;
+	/*
+	 * You can mark a specific enum constant to be the null value instead of the
+	 * first one
+	 */
+	@CdiConfiguration.KeyNullValue
+	PLEASE_IGNORE_ME("");
+
+	String key;
+	Predicate<String> validator;
+
+	CompleteEnum(String key) {
+		this(key, s -> true);
 	}
-	
+
+	CompleteEnum(String key, Predicate<String> validator) {
+		this.key = key;
+		this.validator = validator;
+	}
+
+	/*
+	 * You can specify a method to get a validator for a property: if the
+	 * property is not valid, the Configuration Bean will throw an
+	 * IllegalArgumentException in the initialization method.
+	 */
+	@CdiConfiguration.ConfigValidator
+	public Predicate<String> getValidator() {
+		return validator;
+	}
+
+	/*
+	 * You can specify a method to get a particular string value as a property
+	 * key instead of the generic enum constant name in lowercase.
+	 */
 	@CdiConfiguration.KeyString
 	public String getKeyString() {
-		return this.name().toLowerCase() + ".config";
+		return key;
 	}
 }
