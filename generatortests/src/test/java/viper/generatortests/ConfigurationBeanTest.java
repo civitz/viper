260d48f78fe21c2aa31b64724cf94a30ac389dfe
robypiwa <robypiwa@gmail.com>
2017-03-02 21:28:29 +0100

    Provide a more meaningful test
    The test runs the compiler against the already-compiled CompleteEnum.
    Use the test to launch the annotation processor programmatically.
    
    This test is intended both as a debug method, and a skeleton for future
    tests on variations of the enums.

--- generatortests/src/test/java/viper/generatortests/ConfigurationBeanTest.java
+++ generatortests/src/test/java/viper/generatortests/ConfigurationBeanTest.java
@@ -1,17 +1,23 @@
 package viper.generatortests;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import viper.ConfigurationResolver;
 import viper.generator.ConfigurationKeyProcessor;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Set;
+import java.util.List;
 
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
@@ -23,6 +29,10 @@ import javax.tools.ToolProvider;
 
 
 public class ConfigurationBeanTest {
+	
+	@Rule
+	public TemporaryFolder temp = new TemporaryFolder();
+	
 	@Test
 	public void shouldGeneratedConfigurationBeanClass() throws Exception {
 		String className = CompleteEnum.class.getPackage().getName() + ".ConfigurationBean";
@@ -53,29 +63,61 @@ public class ConfigurationBeanTest {
 			.as("Generated file should be an interface extending " + ConfigurationResolver.class.getSimpleName())
 			.isTrue();
 	}
-	
-     @Test
-     public void runAnnoationProcessor() throws Exception {
-             String source = "src/test/";
 
-             Iterable<JavaFileObject> files = getSourceFiles(source);
+	@Test
+	public void shouldRunAnnotationProcessor() throws Exception {
+		final String enumToCompile = "CompleteEnum.java";
 
-             JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		/*
+		 * Note to future dev: this is intended both as a test and as a way to
+		 * debug future versions of the annotation processor.
+		 * 
+		 * You can use it to test different enum variation, and my hope is to
+		 * extend the tests by putting "java" files in resources directory, and
+		 * manually call the compiler here.
+		 * 
+		 */
+		
+		File sourceDir = temp.newFolder("sources");
+		File outputDir = temp.newFolder("classes");
+		File generatedSourcesDir = temp.newFolder("generated");
 
-             CompilationTask task = compiler.getTask(new PrintWriter(System.out), null, null, null, null, files);
-             task.setProcessors(Arrays.asList(new ConfigurationKeyProcessor()));
+		Path source = Paths.get("src", "test", "java", "viper", "generatortests", enumToCompile);
+		Path destinationDir = Paths.get(sourceDir.getAbsolutePath(), "viper", "generatortests");
+		Files.createDirectories(destinationDir);
+		Files.copy(source, destinationDir.resolve(enumToCompile));
 
-             task.call();
-     }
+		// combine compiler
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+		fileManager.setLocation(StandardLocation.SOURCE_PATH, Arrays.asList(sourceDir));
+		fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(outputDir));
+		fileManager.setLocation(StandardLocation.SOURCE_OUTPUT, Arrays.asList(generatedSourcesDir));
 
-     private Iterable<JavaFileObject> getSourceFiles(String p_path) throws Exception {
-             JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-             StandardJavaFileManager files = compiler.getStandardFileManager(null, null, null);
-             files.setLocation(StandardLocation.SOURCE_PATH, Arrays.asList(new File(p_path)));
+		// get files to be compiled
+		Iterable<JavaFileObject> files = fileManager.list(StandardLocation.SOURCE_PATH, "",
+				Collections.singleton(Kind.SOURCE), true);
 
-             Set<Kind> fileKinds = Collections.singleton(Kind.SOURCE);
-             return files.list(StandardLocation.SOURCE_PATH, "", fileKinds, true);
-     }
+		// setup compilation task
+		CompilationTask task = compiler.getTask(new PrintWriter(System.out), fileManager, null, null, null, files);
+		task.setProcessors(Arrays.asList(new ConfigurationKeyProcessor()));
 
+		// compile
+		Boolean result = task.call();
+
+		assertThat(result)
+			.as("Compile task should work on example enum")
+			.isTrue();
+
+		List<String> javaGeneratedFiles = Files.walk(generatedSourcesDir.toPath())
+			.filter(p->p.toFile().isFile())
+			.filter(p->p.getFileName().toString().endsWith(".java"))
+			.map(p->p.getFileName())
+			.map(p->p.toString())
+			.collect(toList());
+
+		assertThat(javaGeneratedFiles)
+			.contains("ConfigurationBean.java", "Configuration.java", "PropertyFileConfigurationResolver.java");
+	}
 	
 }
