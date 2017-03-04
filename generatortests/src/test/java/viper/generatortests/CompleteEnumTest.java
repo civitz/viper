9ebf5e2efaa1addc38e3b269722d5d6b82814219
robypiwa <robypiwa@gmail.com>
2017-03-04 21:36:27 +0100

    Add more tests for complete enum
    Introduced a simple DSL for testing generated methods in classes

--- generatortests/src/test/java/viper/generatortests/CompleteEnumTest.java
+++ generatortests/src/test/java/viper/generatortests/CompleteEnumTest.java
@@ -3,10 +3,18 @@ package viper.generatortests;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assume.assumeTrue;
+import static viper.generatortests.utils.MethodPredicates.hasAnnotations;
+import static viper.generatortests.utils.MethodPredicates.hasName;
+import static viper.generatortests.utils.MethodPredicates.hasParametersOfType;
+import static viper.generatortests.utils.MethodPredicates.hasReturnType;
+import static viper.generatortests.utils.MethodPredicates.isStatic;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
@@ -15,7 +23,11 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.JavaFileObject;
@@ -24,9 +36,14 @@ import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
+import org.assertj.core.api.Condition;
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runners.MethodSorters;
@@ -40,17 +57,34 @@ public class CompleteEnumTest {
 	@ClassRule
 	public static TemporaryFolder temp = new TemporaryFolder();
 	
+	@Rule
+	public JUnitSoftAssertions softly = new JUnitSoftAssertions();
+	
+	private static final String packageName = CompleteEnum.class.getPackage().getName();
+	
 	public static boolean annotationProcessorHasRunSuccesfully = false;
 
 	private static File sourceDir;
 	private static File outputDir;
 	private static File generatedSourcesDir;
+	
+	URLClassLoader outputClassLoader;
 
 	@BeforeClass
-	public static void setUp() throws Exception{
+	public static void setUpClass() throws Exception{
 		sourceDir = temp.newFolder("sources");
 		outputDir = temp.newFolder("classes");
 		generatedSourcesDir = temp.newFolder("generated");
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		outputClassLoader = getClassLoaderForCompiledFiles();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		outputClassLoader.close();
 	}
 	
 	@Test // starts with "_" for method ordering
@@ -102,6 +136,7 @@ public class CompleteEnumTest {
 			.collect(toList());
 
 		assertThat(javaGeneratedFiles)
+			.as("Chould generate annotation, configuration bean, and property file config resolver")
 			.contains("ConfigurationBean.java", "Configuration.java", "PropertyFileConfigurationResolver.java");
 		annotationProcessorHasRunSuccesfully = true;
 	}
@@ -109,51 +144,140 @@ public class CompleteEnumTest {
 	@Test
 	public void shouldGeneratedConfigurationBeanClass() throws Exception {
 		assumingAnnotationProcessorHasRun();
-		String className = CompleteEnum.class.getPackage().getName() + ".ConfigurationBean";
-		Class<?> confBeanClass = loadClassFromCompiledFiles(className);
+		String className = packageName + ".ConfigurationBean";
+		Class<?> confBeanClass = tryLoadClassFromCompiledFiles(className).orElse(null);
+		assertThat(confBeanClass)
+			.isNotNull()
+			.isNotAnnotation();
+		
+		softly.assertThat(confBeanClass.getDeclaredMethods())
+			.has(aMethodThat("validates a property", 
+					hasName("isValid"), 
+					hasParametersOfType(CompleteEnum.class, String.class),
+					isStatic()))
+			.has(aMethodThat("formats a message for missing properties", 
+					hasName("formatMissing"),
+					hasParametersOfType(CompleteEnum.class),
+					isStatic().negate()))
+			.has(aMethodThat("formats a message for invalid properties", 
+					hasName("formatInvalid"),
+					hasParametersOfType(CompleteEnum.class, String.class),
+					isStatic().negate()));
+	}
+	
+	@Test
+	public void shouldGenerateAllProducerMethods() throws Exception {
+		assumingAnnotationProcessorHasRun();
+		
+		String beanClassName = packageName + ".ConfigurationBean";
+		String annotationClassName = packageName + ".Configuration";
+
+		Optional<Class<?>> beanClass = tryLoadClassFromCompiledFiles(beanClassName);
+		Optional<Class<? extends Annotation>> annotationClass = tryLoadClassFromCompiledFiles(annotationClassName);
+
+		assumeTrue("No Configuration annotation present, skipping test...", annotationClass.isPresent());
+		assumeTrue("No ConfigurationBean class present, skipping test...", beanClass.isPresent());
+		
+
+		final Class<? extends Annotation> generatedAnnotation = annotationClass.get();
+		
+		softly.assertThat(beanClass.get().getDeclaredMethods())
+			.has(aCdiProducerMethod(generatedAnnotation, "getStringProperty", String.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getByteProperty", Byte.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getShortProperty", Short.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getIntegerProperty", Integer.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getLongProperty", Long.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getFloatProperty", Float.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getDoubleProperty", Double.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getBooleanProperty", Boolean.class))
+			.has(aCdiProducerMethod(generatedAnnotation, "getCharacterProperty", Character.class));
+	}
+	
+	@Test
+	public void shouldGeneratedConfigurationAnnotation() throws Exception {
+		assumingAnnotationProcessorHasRun();
+		String className = packageName + ".Configuration";
+		Class<?> confBeanClass = tryLoadClassFromCompiledFiles(className).orElse(null);
+		assertThat(confBeanClass)
+			.isAnnotation()
+			.isNotNull();
+		
+		softly.assertThat(confBeanClass.getDeclaredMethods())
+			.filteredOn(m -> m.getName().equals("value"))
+			.filteredOn(m -> m.getParameterCount() == 0)
+			.filteredOn(m -> m.getReturnType().equals(CompleteEnum.class))
+			.as("Configuration annotation should have a value method of type " + CompleteEnum.class.getName())
+			.hasSize(1);
+	}
+	
+	@Test
+	public void shouldGeneratedPropertyFileConfigurationResolver() throws Exception {
+		assumingAnnotationProcessorHasRun();
+		String className = packageName + ".PropertyFileConfigurationResolver";
+		Class<?> confBeanClass = tryLoadClassFromCompiledFiles(className).orElse(null);
 		assertThat(confBeanClass)
 			.isNotAnnotation()
 			.isNotNull();
+		
+		softly.assertThat(ConfigurationResolver.class.isAssignableFrom(confBeanClass))
+			.as("Generated file should be an interface extending " + ConfigurationResolver.class.getSimpleName())
+			.isTrue();
+		
+		softly.assertThat(confBeanClass.getDeclaredMethods())
+			.has(aMethodThat("gets the configuration value from an enum constant", 
+					hasName("getConfigurationValue"),
+					isStatic().negate(),
+					hasParametersOfType(CompleteEnum.class),
+					hasReturnType(String.class)));
 	}
 
-	public Class<?> loadClassFromCompiledFiles(String className) throws ClassNotFoundException, IOException {
-		try (URLClassLoader classLoader = new URLClassLoader(new URL[] { outputDir.toURI().toURL() })) {
-			Class<?> confBeanClass = classLoader.loadClass(className);
-			return confBeanClass;
+	@SafeVarargs
+	static Condition<Method[]> aMethodThat(String description, Predicate<Method> first, Predicate<Method>... predicates){
+		final Predicate<Method> list = Arrays.stream(predicates)
+				.reduce(first, Predicate::and);
+		Predicate<Method[]> combined = methods -> Arrays.stream(methods)
+					.filter(list)
+					.count() == 1;
+		return new Condition<Method[]>(combined, "a method that " + description);
+	}
+	
+	public Condition<Method[]> aCdiProducerMethod(final Class<? extends Annotation> generatedAnnotation,
+			final String methodName, final Class<?> producerType) {
+		return aMethodThat("produces "+ producerType.getSimpleName() + " properties", 
+				hasName(methodName),
+				hasParametersOfType(InjectionPoint.class),
+				hasReturnType(producerType),
+				hasAnnotations(Produces.class, generatedAnnotation));
+	}
+
+	public URLClassLoader getClassLoaderForCompiledFiles() throws MalformedURLException {
+		return new URLClassLoader(new URL[] { outputDir.toURI().toURL() });
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T>  Class<? extends T> loadClassFromCompiledFiles(String className) throws ClassNotFoundException, IOException {
+		return (Class<? extends T>) outputClassLoader.loadClass(className);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> Optional<Class<? extends T>> tryLoadClassFromCompiledFiles(String className) throws ClassNotFoundException, IOException {
+		try {
+			return Optional.of((Class<? extends T>)loadClassFromCompiledFiles(className));
+		}catch(ClassNotFoundException e){
+			return Optional.empty();
 		}
 	}
 
 	/*
 	 * I know, this method is a really ugly way to skip tests if annotation
-	 * processor hasn't run. But it works.
+	 * processor didn't run. But it works.
+	 * Also, I tried with JExample, but it didn't work well with @ClassRule
 	 */
 	public void assumingAnnotationProcessorHasRun() {
 		assumeTrue("Annotation should have run succesfully for this test to run...",
 				annotationProcessorHasRunSuccesfully);
 	}
 	
-	@Test
-	public void shouldGeneratedConfigurationAnnotation() throws Exception {
-		assumingAnnotationProcessorHasRun();
-		String className = CompleteEnum.class.getPackage().getName() + ".Configuration";
-		Class<?> confBeanClass = loadClassFromCompiledFiles(className);
-		assertThat(confBeanClass)
-			.isAnnotation()
-			.isNotNull();
-	}
 	
-	@Test
-	public void shouldGeneratedPropertyFileConfigurationResolver() throws Exception {
-		assumingAnnotationProcessorHasRun();
-		String className = CompleteEnum.class.getPackage().getName() + ".PropertyFileConfigurationResolver";
-		Class<?> confBeanClass = loadClassFromCompiledFiles(className);
-		assertThat(confBeanClass)
-			.isNotAnnotation()
-			.isNotNull();
-		
-		assertThat(ConfigurationResolver.class.isAssignableFrom(confBeanClass))
-			.as("Generated file should be an interface extending " + ConfigurationResolver.class.getSimpleName())
-			.isTrue();
-	}
 	
 }
